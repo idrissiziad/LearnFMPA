116d01bfb82d94255046301800702c4fc03468dd
idrissiziad <idrissiziad7@gmail.com>
2025-12-26 00:05:57 +0100

    modified:   src/app/modules/[id]/page.tsx

--- src/app/modules/[id]/page.tsx
+++ src/app/modules/[id]/page.tsx
@@ -724,7 +724,7 @@ export default function ModulePage() {
                   Correct
                 </div>
               )}
-              {isCorrectlyAnswered && sessionFilter === 'Toutes les sessions' && currentQuestion?.year && (
+              {sessionFilter === 'Toutes les sessions' && currentQuestion?.year && (
                 <div className={`px-2 py-0.5 sm:px-3 sm:py-1 rounded-full text-[10px] sm:text-sm font-medium flex items-center ${isDarkMode ? 'bg-purple-900 text-purple-300' : 'bg-purple-100 text-purple-700'}`}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 sm:h-4 sm:w-4 mr-0.5 sm:mr-1" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
