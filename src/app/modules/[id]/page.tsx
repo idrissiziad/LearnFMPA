eb91b41d199fb3f9af7a4fe4fd23443dea8f1625
idrissiziad <idrissiziad7@gmail.com>
2025-12-26 00:39:53 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/modules/[id]/page.tsx

--- src/app/modules/[id]/page.tsx
+++ src/app/modules/[id]/page.tsx
@@ -823,7 +823,7 @@ export default function ModulePage() {
                 >
                   <button
                     onClick={() => handleAnswerSelect(index)}
-                    className="w-full text-left focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 rounded-lg"
+                    className="w-full text-left focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg"
                     disabled={showAnswer}
                   >
                     <div className="flex items-start">
